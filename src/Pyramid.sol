7a7808b4aa86c79b677785cddea7ef2265b034f2
kimziwu <jiwoo.kim@dream-academy.io>
2023-03-12 20:42:20 +0900

    first

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
 contract Pyramid {
-    function run(uint size) pure public returns (string memory _out) {
+    function run(uint size) public returns (string memory _out) {
+
         _out = ""; // TODO
         /* Example (size = 5):
          *
@@ -13,5 +15,12 @@ contract Pyramid {
          *     "*****\n"
          *
          */
+         for (uint i=0; i<size; i++){
+            for (uint j=0; j<=i; j++){
+                _out=string.concat(_out,"*");
+            }
+            _out=string.concat(_out,"\n");
+         }
+         console.log("result : \n%s",_out);
     }
 }
